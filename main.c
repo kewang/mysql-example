268fffaa4bc184f9e4f68a3846519a05f6e68ef1
kewang <cpckewang@gmail.com>
2015-06-20 00:29:46 +0800

    add select query statement (success)

--- main.c
+++ main.c
@@ -27,5 +27,66 @@ int main(){
   printf("MySQL Client Info: %s \n", mysql_get_client_info());
   printf("MySQL Server Info: %s \n", mysql_get_server_info(MySQLConnection));
 
+  int mysqlStatus = 0;
+  MYSQL_RES *mysqlResult = NULL;
+
+  if(mysqlResult){
+    mysql_free_result(mysqlResult);
+    mysqlResult = NULL;
+  }
+
+  MYSQL_ROW mysqlRow;
+  MYSQL_FIELD *mysqlFields;
+  my_ulonglong numRows;
+  unsigned int numFields;
+
+  char *sqlSelStatement = "select * from user";
+
+  mysqlStatus = mysql_query(MySQLConnection, sqlSelStatement);
+
+  if(mysqlStatus){
+    printf((char *)mysql_error(MySQLConnection));
+
+    return 1;
+  }
+
+  mysqlResult = mysql_store_result(MySQLConnection);
+
+  if(mysqlResult){
+    numRows = mysql_num_rows(mysqlResult);
+
+    numFields = mysql_field_count(MySQLConnection);
+
+    numFields = mysql_num_fields(mysqlResult);
+
+    printf("Number of rows=%u  Number of fields=%u \n",numRows,numFields);
+  }else{
+    printf("Result set is empty\n");
+  }
+
+  mysqlFields = mysql_fetch_fields(mysqlResult);
+
+  for(int i = 0; i < numFields; i++){
+    printf("%s\t", mysqlFields[i].name);
+  }
+
+  printf("\n");
+
+  while(mysqlRow = mysql_fetch_row(mysqlResult)){
+    for(int i = 0; i < numFields; i++){
+      printf("%s\t", mysqlRow[i] ? mysqlRow[i] : "NULL");
+    }
+
+    printf("\n");
+  }
+
+  if(mysqlResult){
+    mysql_free_result(mysqlResult);
+
+    mysqlResult = NULL;
+  }
+
+  mysql_close(MySQLConnection);
+
   return 0;
 }
