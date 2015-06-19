7eee2a3d7f580ccf327718c290bdc217fb83d8ce
kewang <cpckewang@gmail.com>
2015-06-20 00:16:30 +0800

    add mysql connection info

--- main.c
+++ main.c
@@ -2,5 +2,30 @@
 #include <mysql.h>
 
 int main(){
-  printf("hello world\n");
+  MYSQL *MySQLConRet;
+  MYSQL *MySQLConnection = NULL;
+
+  char *hostname = "localhost";
+  char *user = "root";
+  char password[20];
+  char *db = "hedistest";
+
+  MySQLConnection = mysql_init(NULL);
+
+  printf("enter password: ");
+  scanf("%s", password);
+
+  MySQLConRet = mysql_real_connect(MySQLConnection, hostname, user, password, db, 0, NULL, 0);
+
+  if(MySQLConRet == NULL){
+    printf("fail\n");
+
+    return 1;
+  }
+
+  printf("MySQL Connection Info: %s \n", mysql_get_host_info(MySQLConnection));
+  printf("MySQL Client Info: %s \n", mysql_get_client_info());
+  printf("MySQL Server Info: %s \n", mysql_get_server_info(MySQLConnection));
+
+  return 0;
 }
